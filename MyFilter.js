c1219fb2b4bfa19b0f4a5160d137b1881cf1630c
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-03-29 17:08:56 +0530

    Update MyFilter.js

--- MyFilter.js
+++ MyFilter.js
@@ -5,63 +5,88 @@ $(document).ready(function(){
 		if ($('#checkKashayp').is(":checked") || $('#checkKhalap').is(":checked") || $('#checkMandav').is(":checked")
 			|| $('#checkGahilam').is(":checked") || $('#checkGautam').is(":checked") || $('#checkLokaksha').is(":checked")) {
 			if ($('#checkKashayp').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Kashayp").hide();
 			
 			if ($('#checkKhalap').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Khalap").hide();
 			
 			if ($('#checkMandav').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Mandav").hide();
 			
 			if ($('#checkGahilam').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Gahilam").hide();
 			
 			if ($('#checkGautam').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Gautam").hide();
 			
 			if ($('#checkLokaksha').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Lokaksha").hide();
 		} else {
-			alert("1");
+			console.log('all gotra false');
 		}
 		
 		if ($('#checkPune').is(":checked") || $('#checkMumbai').is(":checked") || $('#chceckNagpur').is(":checked")
 			|| $('#checkNasik').is(":checked")) {
 			//pune
 			if ($('#checkPune').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Pune").hide();
 								
 			//Mumbai
 			if ($('#checkMumbai').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Mumbai").hide();
 			
 			//Nagpur
 			if ($('#checkNagpur').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Nagpur").hide();
 			
 			//Nasik
-			if ($('#checkNasik').is(":checked")) 
+			if ($('#checkNasik').is(":checked"))
+				console.log('your message');
+			else			
 				$(".Nasik").hide();
 			
 		} else {
-			alert("2");
+			console.log('all City false');
 		}
 		
 		if ($('#checkDoctor').is(":checked") || $('#checkEngineer').is(":checked") || $('#checkCA').is(":checked")) {
 			//pune
 			if ($('#checkDoctor').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Doctor").hide();
 								
 			//Mumbai
 			if ($('#checkEngineer').is(":checked")) 
+				console.log('your message');
+			else
 				$(".Engineer").hide();
 			
 			//Nagpur
 			if ($('#checkCA').is(":checked")) 
+				console.log('your message');	
+			else
 				$(".CA").hide();
-			
 		} else {
-			alert("3");
+			console.log('all occupation false');
 		}
 	});
 });
