4cc7b75a48514af00103605a2f0c0a2087933fe7
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-03-29 12:59:26 +0530

    v1

---
+++ MyFilter.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+				
+				$(".custom-control-input").click(function(){
+					
+					//pune
+					if ($('#checkPune').is(":checked")) 
+						$(".Pune").show();
+					else 
+						$(".Pune").hide();
+					
+					//Mumbai
+					if ($('#checkMumbai').is(":checked")) 
+						$(".Mumbai").show();
+					else 
+						$(".Mumbai").hide();	
+					
+					//Nagpur
+					if ($('#checkNagpur').is(":checked")) 
+						$(".Nagpur").show();
+					else 
+						$(".Nagpur").hide();	
+					
+					//Nasik
+					if ($('#checkNasik').is(":checked")) 
+						$(".Nasik").show();
+					else 
+						$(".Nasik").hide();	
+					
+					if ($('#checkKashayp').is(":checked")) 
+						$(".Kashayp").show();
+					else 
+						$(".Kashayp").hide();	
+					
+					if ($('#checkKhalap').is(":checked")) 
+						$(".Khalap").show();
+					else 
+						$(".Khalap").hide();	
+					
+					if ($('#checkMandav').is(":checked")) 
+						$(".Mandav").show();
+					else 
+						$(".Mandav").hide();	
+					
+					if ($('#checkGahilam').is(":checked")) 
+						$(".Gahilam").show();
+					else 
+						$(".Gahilam").hide();	
+					
+					if ($('#checkGautam').is(":checked")) 
+						$(".Gautam").show();
+					else 
+						$(".Gautam").hide();	
+					
+					//Lokaksh
+					if ($('#checkLokaksh').is(":checked")) 
+						$(".Lokaksh").show();
+					else 
+						$(".Lokaksh").hide();
+					
+					//Doctor
+					if ($('#checkDoctor').is(":checked")) 
+						$(".Doctor").show();
+					else 
+						$(".Doctor").hide();
+					
+					//Engineer
+					if ($('#checkEngineer').is(":checked")) 
+						$(".Engineer").show();
+					else 
+						$(".Engineer").hide();
+					
+					//CA
+					if ($('#checkCA').is(":checked")) 
+						$(".CA").show();
+					else 
+						$(".CA").hide();
+					
+				});
+			
+				
+				
+			});
