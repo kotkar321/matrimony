5cc27ba9e6b5970e5f17db6f1d2f993ca1c8fb8c
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-03-29 16:13:56 +0530

    Update MyFilter.js

--- MyFilter.js
+++ MyFilter.js
@@ -1,29 +1,28 @@
 $(document).ready(function(){
 	$(".custom-control-input").click(function(){
-		$(".all").fadeIn(3000);
-		
+		$(".all").show();
 		var gotra;
 		if (!$('#checkKashayp').is(":checked") && !$('#checkKhalap').is(":checked") && !$('#checkMandav').is(":checked")
 			&& !$('#checkGahilam').is(":checked") && !$('#checkGautam').is(":checked") && !$('#checkLokaksha').is(":checked")) {
 			gotra = true;
 		} else {
 			if (!$('#checkKashayp').is(":checked")) 
-				$(".Kashayp").fadeOut("slow");
+				$(".Kashayp").fadeOut(1500);
 			
 			if (!$('#checkKhalap').is(":checked")) 
-				$(".Khalap").fadeOut("slow");
+				$(".Khalap").fadeOut(1500);
 			
 			if (!$('#checkMandav').is(":checked")) 
-				$(".Mandav").fadeOut("slow");
+				$(".Mandav").fadeOut(1500);
 			
 			if (!$('#checkGahilam').is(":checked")) 
-				$(".Gahilam").fadeOut("slow");
+				$(".Gahilam").fadeOut(1500);
 			
 			if (!$('#checkGautam').is(":checked")) 
-				$(".Gautam").fadeOut("slow");
+				$(".Gautam").fadeOut(1500);
 			
 			if (!$('#checkLokaksha').is(":checked")) 
-				$(".Lokaksha").fadeOut("slow");
+				$(".Lokaksha").fadeOut(1500);
 		}
 		
 		if (!$('#checkPune').is(":checked") && !$('#checkMumbai').is(":checked") && !$('#chceckNagpur').is(":checked")
@@ -32,19 +31,19 @@ $(document).ready(function(){
 		} else {
 			//pune
 			if (!$('#checkPune').is(":checked")) 
-				$(".Pune").fadeOut("slow");
+				$(".Pune").fadeOut(1500);
 								
 			//Mumbai
 			if (!$('#checkMumbai').is(":checked")) 
-				$(".Mumbai").fadeOut("slow");
+				$(".Mumbai").fadeOut(1500);
 			
 			//Nagpur
 			if (!$('#checkNagpur').is(":checked")) 
-				$(".Nagpur").fadeOut("slow");
+				$(".Nagpur").fadeOut(1500);
 			
 			//Nasik
 			if (!$('#checkNasik').is(":checked")) 
-				$(".Nasik").fadeOut("slow");
+				$(".Nasik").fadeOut(1500);
 			
 		}
 		
@@ -53,15 +52,15 @@ $(document).ready(function(){
 		} else {
 			//pune
 			if (!$('#checkDoctor').is(":checked")) 
-				$(".Doctor").fadeOut("slow");
+				$(".Doctor").fadeOut(1500);
 								
 			//Mumbai
 			if (!$('#checkEngineer').is(":checked")) 
-				$(".Engineer").fadeOut("slow");
+				$(".Engineer").fadeOut(1500);
 			
 			//Nagpur
 			if (!$('#checkCA').is(":checked")) 
-				$(".CA").fadeOut("slow");
+				$(".CA").fadeOut(1500);
 		}
 	});
 });
