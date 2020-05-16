16a49ab5387f252c45fedf1c09bacc38e5c49e91
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-17 00:48:25 +0530

    Update MyFilter.js

--- MyFilter.js
+++ MyFilter.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	
 	var includes = $('[data-include]');
 	    jQuery.each(includes, function(){
-	      var file = 'views/' + $(this).data('include') + '.html';
+	      var file =  $(this).data('include') + '.html';
 	      $(this).load(file);
 	    });
 	
