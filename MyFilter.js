62900a5642f679a55f336732915230d901208bff
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-17 01:29:09 +0530

    Update MyFilter.js

--- MyFilter.js
+++ MyFilter.js
@@ -2,14 +2,11 @@ $(document).ready(function(){
 	
 	$(function(){
 		var includes = $('[data-include]');
-		var limit = 10005;
 		jQuery.each(includes, function(){
-		  for (i = 10001; i < limit; i++) {
-			var file =  i + '.html';
-			$(this).load(file);
-		}
+		  var file = $(this).data('include') + '.html';
+		  $(this).load(file);
 		});
-	});
+	  });
 	
 	
 	$(".custom-control-input").click(function(){
