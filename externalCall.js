6fb9b161e368828a3197854e92b92b056131a3a4
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-21 18:35:25 +0530

    Update externalCall.js

--- externalCall.js
+++ externalCall.js
@@ -16,10 +16,11 @@
 				    $.each(results, function(index, element){
 					    console.log(element);
 					    var values = element.split(",");
-					    if(values[1] === undefined || values[1] == 'NAME') {
+					    if(values[1] === undefined || values[1] == 'Name') {
 						    
 					    }else {
 					    var myStr = "<div class='col-md-4 GOTRA OCCUPATION CITY all'><div class='row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'><div class='col p-3 d-flex flex-column position-static'><strong class='d-inline-block mb-2 text-success'>TYPE</strong><h3 class='mb-0'>NAME</h3><div class='mb-1 text-muted'>BIRTHDATE</div><div class=''>Height : <b><i>HEIGHT</i></b> </div><div class=''>Gotra : <b><i>GOTRA</i></b> </div><div class=''>Occupation : <b><i>OCCUPATION</i></b></div><div class=''>Work City : <b><i>CITY </i></b></div><a href='URL' class='stretched-link'>Download Bio Data</a></div></div></div>";
+					    myStr = replaceAll(myStr, 'TYPE', fileName);	    
 					    myStr = replaceAll(myStr, 'NAME', values[1]);	    
 				   	    myStr = replaceAll(myStr, 'BIRTHDATE', values[2]);
 				 	    myStr = replaceAll(myStr, 'GOTRA', values[3]);
@@ -34,6 +35,6 @@
 				    document.getElementById("profiles").innerHTML = finalDataString;
 			    }
 			  };
-			  xhttp.open("GET", fileName, true);
+			  xhttp.open("GET", fileName + ".csv", true);
 			  xhttp.send();
 			}
