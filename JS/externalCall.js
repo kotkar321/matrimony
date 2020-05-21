78eda7b8d2f661690a145f0d24d682b3b1c778b4
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-21 23:34:58 +0530

    Update externalCall.js

--- JS/externalCall.js
+++ JS/externalCall.js
@@ -16,25 +16,13 @@
 				    $.each(results, function(index, element){
 					    console.log(element);
 					    var values = element.split("=");
-					    if(values[1] === undefined) {
+					    if(values[0] === undefined) {
 						    
 					    }else if(values[0] == "siteName") {
-						    
-						}
-					    var myStr = "<div class='col-md-4 GOTRA OCCUPATION CITY all'><div class='row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'><div class='col p-4 d-flex flex-column position-static'><strong class='d-inline-block text-success'>TYPE</strong><h3 class='mb-0'>NAME</h3><div class='mb-1 text-muted'>BIRTHDATE</div><div class=''>Height : <b><i>HEIGHT</i></b> </div><div class=''>Gotra : <b><i>GOTRA</i></b> </div><div class=''>Occupation : <b><i>OCCUPATION</i></b></div><div class=''>Work City : <b><i>CITY </i></b></div><a href='URL' class='stretched-link'>Download Bio Data</a></div></div></div>";
-					    myStr = replaceAll(myStr, 'TYPE', fileName);	    
-					    myStr = replaceAll(myStr, 'NAME', values[1]);	    
-				   	    myStr = replaceAll(myStr, 'BIRTHDATE', values[2]);
-				 	    myStr = replaceAll(myStr, 'GOTRA', values[3]);
-				            myStr = replaceAll(myStr, 'OCCUPATION', values[4]);
-					    myStr = replaceAll(myStr, 'CITY', values[5]);
-					    myStr = replaceAll(myStr, 'HEIGHT', values[6]);
-					    myStr = replaceAll(myStr, 'URL', values[7]);
-					    finalDataString = finalDataString + myStr;
+						    $("#siteName").val(values[1]);
+					    } else {
 					    }
 				    });
-				    //document.getElementById("demo").innerHTML = this.responseText;
-				    document.getElementById("profiles").innerHTML = finalDataString;
 			    }
 			  };
 			  xhttp.open("GET", "CSV/" + fileName + ".csv", true);
@@ -55,16 +43,16 @@
 					    if(values[1] === undefined || values[1] == 'Name') {
 						    
 					    }else {
-					    var myStr = "<div class='col-md-4 GOTRA OCCUPATION CITY all'><div class='row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'><div class='col p-4 d-flex flex-column position-static'><strong class='d-inline-block text-success'>TYPE</strong><h3 class='mb-0'>NAME</h3><div class='mb-1 text-muted'>BIRTHDATE</div><div class=''>Height : <b><i>HEIGHT</i></b> </div><div class=''>Gotra : <b><i>GOTRA</i></b> </div><div class=''>Occupation : <b><i>OCCUPATION</i></b></div><div class=''>Work City : <b><i>CITY </i></b></div><a href='URL' class='stretched-link'>Download Bio Data</a></div></div></div>";
-					    myStr = replaceAll(myStr, 'TYPE', fileName);	    
-					    myStr = replaceAll(myStr, 'NAME', values[1]);	    
-				   	    myStr = replaceAll(myStr, 'BIRTHDATE', values[2]);
-				 	    myStr = replaceAll(myStr, 'GOTRA', values[3]);
-				            myStr = replaceAll(myStr, 'OCCUPATION', values[4]);
-					    myStr = replaceAll(myStr, 'CITY', values[5]);
-					    myStr = replaceAll(myStr, 'HEIGHT', values[6]);
-					    myStr = replaceAll(myStr, 'URL', values[7]);
-					    finalDataString = finalDataString + myStr;
+						    var myStr = "<div class='col-md-4 GOTRA OCCUPATION CITY all'><div class='row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'><div class='col p-4 d-flex flex-column position-static'><strong class='d-inline-block text-success'>TYPE</strong><h3 class='mb-0'>NAME</h3><div class='mb-1 text-muted'>BIRTHDATE</div><div class=''>Height : <b><i>HEIGHT</i></b> </div><div class=''>Gotra : <b><i>GOTRA</i></b> </div><div class=''>Occupation : <b><i>OCCUPATION</i></b></div><div class=''>Work City : <b><i>CITY </i></b></div><a href='URL' class='stretched-link'>Download Bio Data</a></div></div></div>";
+						    myStr = replaceAll(myStr, 'TYPE', fileName);	    
+						    myStr = replaceAll(myStr, 'NAME', values[1]);	    
+						    myStr = replaceAll(myStr, 'BIRTHDATE', values[2]);
+						    myStr = replaceAll(myStr, 'GOTRA', values[3]);
+						    myStr = replaceAll(myStr, 'OCCUPATION', values[4]);
+						    myStr = replaceAll(myStr, 'CITY', values[5]);
+						    myStr = replaceAll(myStr, 'HEIGHT', values[6]);
+						    myStr = replaceAll(myStr, 'URL', values[7]);
+						    finalDataString = finalDataString + myStr;
 					    }
 				    });
 				    //document.getElementById("demo").innerHTML = this.responseText;
