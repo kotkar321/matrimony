f2ad9f64ea885f744ca745c4595f12785bd6f424
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-21 18:32:57 +0530

    Update externalCall.js

--- externalCall.js
+++ externalCall.js
@@ -19,7 +19,7 @@
 					    if(values[1] === undefined || values[1] == 'NAME') {
 						    
 					    }else {
-					    var myStr = "<div class='col-md-4 GOTRA OCCUPATION CITY all'><div class='row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'><div class='col p-4 d-flex flex-column position-static'><strong class='d-inline-block mb-2 text-success'>TYPE</strong><h3 class='mb-0'>NAME</h3><div class='mb-1 text-muted'>BIRTHDATE</div><div class=''>Height : <b><i>HEIGHT</i></b> </div><div class=''>Gotra : <b><i>GOTRA</i></b> </div><div class=''>Occupation : <b><i>OCCUPATION</i></b></div><div class=''>Work City : <b><i>CITY </i></b></div><a href='URL' class='stretched-link'>Download Bio Data</a></div></div></div>";
+					    var myStr = "<div class='col-md-4 GOTRA OCCUPATION CITY all'><div class='row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'><div class='col p-3 d-flex flex-column position-static'><strong class='d-inline-block mb-2 text-success'>TYPE</strong><h3 class='mb-0'>NAME</h3><div class='mb-1 text-muted'>BIRTHDATE</div><div class=''>Height : <b><i>HEIGHT</i></b> </div><div class=''>Gotra : <b><i>GOTRA</i></b> </div><div class=''>Occupation : <b><i>OCCUPATION</i></b></div><div class=''>Work City : <b><i>CITY </i></b></div><a href='URL' class='stretched-link'>Download Bio Data</a></div></div></div>";
 					    myStr = replaceAll(myStr, 'NAME', values[1]);	    
 				   	    myStr = replaceAll(myStr, 'BIRTHDATE', values[2]);
 				 	    myStr = replaceAll(myStr, 'GOTRA', values[3]);
