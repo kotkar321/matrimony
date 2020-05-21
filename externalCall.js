69f9ae8cd28858ee4ef501c9a28801f92a06768f
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-21 18:04:19 +0530

    Update externalCall.js

--- externalCall.js
+++ externalCall.js
@@ -19,10 +19,14 @@
 					    if(values[1] === undefined) {
 						    
 					    }else {
-					    var myStr = "<div class='col-md-4 CITY GOTRA OCCUPATION all'><div class='row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'><div class='col p-4 d-flex flex-column position-static'><strong class='d-inline-block mb-2 text-success'>TYPE</strong><h3 class='mb-0'>NAME</h3><div class='mb-1 text-muted'>BIRTHDATE</div><div class='mb-1'>Gotra : <b><i>GOTRA</i></b> </div><div class='mb-1'>Occupation : <b><i>OCCUPATION</i></b></div><div class='mb-1'>Work City : <b><i>CITY </i></b></div><a href='URL' class='stretched-link'>Download Bio Data</a></div></div></div>";
-					    myStr = replaceAll(myStr, 'CITY', values[3]);
-					    myStr = replaceAll(myStr, 'OCCUPATION', values[2]);
+					    var myStr = "<div class='col-md-4 GOTRA OCCUPATION CITY all'><div class='row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'><div class='col p-4 d-flex flex-column position-static'><strong class='d-inline-block mb-2 text-success'>TYPE</strong><h3 class='mb-0'>NAME</h3><div class='mb-1 text-muted'>BIRTHDATE</div><div class='mb-1'>Gotra : <b><i>GOTRA</i></b> </div><div class='mb-1'>Occupation : <b><i>OCCUPATION</i></b></div><div class='mb-1'>Work City : <b><i>CITY </i></b></div><a href='URL' class='stretched-link'>Download Bio Data</a></div></div></div>";
 					    myStr = replaceAll(myStr, 'NAME', values[1]);	    
+				   	    myStr = replaceAll(myStr, 'BIRTHDATE', values[2]);
+				 	    myStr = replaceAll(myStr, 'GOTRA', values[3]);
+				            myStr = replaceAll(myStr, 'OCCUPATION', values[4]);
+					    myStr = replaceAll(myStr, 'CITY', values[5]);
+					    myStr = replaceAll(myStr, 'HEIGHT', values[6]);
+					    myStr = replaceAll(myStr, 'URL', values[7]);
 					    finalDataString = finalDataString + myStr;
 					    }
 				    });
