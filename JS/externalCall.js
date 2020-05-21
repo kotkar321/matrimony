2a9ac40521ba23a448fadcc0c3c74fe1655acf46
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-22 01:51:32 +0530

    Update externalCall.js

--- JS/externalCall.js
+++ JS/externalCall.js
@@ -17,9 +17,10 @@
 					    var values = element.split("=");
 					    if(values[0] === undefined || values[0] == "") {
 						    
-					    } else if(values[2] === undefined || values[2] == "") {						    
-					    	document.getElementById(values[0]).innerHTML = values[1];
 					    } else {						    
+					    	document.getElementById(values[0]).innerHTML = values[1];
+					    } 
+					    if(values[2] !== undefined && values[2] != "") {						    
 					    	document.getElementById(values[0]).href=values[2]; 
 					    }
 					    
