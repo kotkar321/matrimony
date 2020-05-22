2602845434f787b5567f30817c55f028933d8a06
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-22 20:47:14 +0530

    Update mainCalls.js

--- JS/mainCalls.js
+++ JS/mainCalls.js
@@ -1,1 +1,27 @@
+function loadHomePageDetails(typeFile, langFile) {
+   
+}
 
+function loadGroomPageDetails(typeFile, langFile) {
+   
+}
+
+function loadBridePageDetails(typeFile, langFile) {
+   
+}
+
+function loadSamajPageDetails(typeFile, langFile) {
+   
+}
+
+function loadContactPageDetails(typeFile, langFile) {
+   
+}
+
+function loadNewsPageDetails(typeFile, langFile) {
+   
+}
+
+function loadHelpPageDetails(typeFile, langFile) {
+   
+}
