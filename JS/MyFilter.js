de0cd1bb25332aa3a6e7c32b198bcbffeb758cbb
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-22 18:06:22 +0530

    Update MyFilter.js

--- JS/MyFilter.js
+++ JS/MyFilter.js
@@ -2,164 +2,194 @@ $(document).ready(function(){
 	$(".custom-control-input").click(function(){
 		$(".all").show();
 		if ($('#GotraFilter0').is(":checked") || $('#GotraFilter1').is(":checked") || $('#GotraFilter2').is(":checked") || $('#GotraFilter3').is(":checked") || $('#GotraFilter4').is(":checked") || $('#GotraFilter5').is(":checked")) {
-			if ($('#GotraFilter0').is(":checked")) 
-				//console.log('your message');
-			else
+			if ($('#GotraFilter0').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".GotraFilter0").hide();
+			}
 			
-			if ($('#GotraFilter1').is(":checked")) 
-				//console.log('your message');
-			else
+			if ($('#GotraFilter1').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".GotraFilter1").hide();
+			}
 			
-			if ($('#GotraFilter2').is(":checked")) 
-				//console.log('your message');
-			else
+			if ($('#GotraFilter2').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".GotraFilter2").hide();
+			}
 			
-			if ($('#GotraFilter3').is(":checked")) 
-				//console.log('your message');
-			else
+			if ($('#GotraFilter3').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".GotraFilter3").hide();
+			}
 			
-			if ($('#GotraFilter4').is(":checked")) 
-				//console.log('your message');
-			else
+			if ($('#GotraFilter4').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".GotraFilter4").hide();
+			}
 			
-			if ($('#GotraFilter5').is(":checked")) 
-				//console.log('your message');
-			else
+			if ($('#GotraFilter5').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".GotraFilter5").hide();
+			}
 		} else {
-			//console.log('all gotra false');
+			console.log('all gotra false');
 		}
 		
 		if ($('#cityFilter0').is(":checked") || $('#cityFilter1').is(":checked") || $('#cityFilter2').is(":checked") || $('#cityFilter3').is(":checked") || $('#cityFilter4').is(":checked") || $('#cityFilter5').is(":checked") || $('#cityFilter6').is(":checked") || $('#cityFilter7').is(":checked") || $('#cityFilter8').is(":checked") || $('#cityFilter9').is(":checked") || $('#cityFilter10').is(":checked")) {
-			if ($('#cityFilter0').is(":checked")) 
-				//console.log('your message');
-			else
+			if ($('#cityFilter0').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".cityFilter0").hide();
-								
-			if ($('#cityFilter1').is(":checked")) 
-				//console.log('your message');
-			else
+			}			
+			
+			if ($('#cityFilter1').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".cityFilter1").hide();
-			
-			if ($('#cityFilter2').is(":checked")) 
-				//console.log('your message');
-			else
+			}
+			if ($('#cityFilter2').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".cityFilter2").hide();
+			}
 			
-			if ($('#cityFilter3').is(":checked"))
-				//console.log('your message');
-			else			
+			if ($('#cityFilter3').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".cityFilter3").hide();
+			}
 			
-			if ($('#cityFilter4').is(":checked"))
-				//console.log('your message');
-			else			
+			if ($('#cityFilter4').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".cityFilter4").hide();
-				
-			if ($('#cityFilter5').is(":checked"))
-				//console.log('your message');
-			else			
+			}
+			
+			if ($('#cityFilter5').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".cityFilter5").hide();
-
-			if ($('#cityFilter6').is(":checked"))
-				//console.log('your message');
-			else			
+			}
+			
+			if ($('#cityFilter6').is(":checked")) {
+				console.log('your message');
+			} else {			
 				$(".cityFilter6").hide();
-
-			if ($('#cityFilter7').is(":checked"))
-				//console.log('your message');
-			else			
+			}
+			
+			if ($('#cityFilter7').is(":checked")) {
+				console.log('your message');
+			} else {			
 				$(".cityFilter7").hide();	
-		
-			if ($('#cityFilter8').is(":checked"))
-				//console.log('your message');
-			else			
+			}
+			
+			if ($('#cityFilter8').is(":checked")) {
+				console.log('your message');
+			} else {			
 				$(".cityFilter8").hide();
-				
-			if ($('#cityFilter9').is(":checked"))
-				//console.log('your message');
-			else			
+			}
+			
+			if ($('#cityFilter9').is(":checked")) {
+				console.log('your message');
+			} else {			
 				$(".cityFilter9").hide();
-				
-			if ($('#cityFilter10').is(":checked"))
-				//console.log('your message');
-			else			
+			}
+			
+			if ($('#cityFilter10').is(":checked")) {
+				console.log('your message');
+			} else {			
 				$(".cityFilter10").hide();	
-				
+			}
+			
 		} else {
-			//console.log('all City false');
+			console.log('all City false');
 		}
 		
 		if ($('#occupationFilter0').is(":checked") || $('#occupationFilter1').is(":checked") || $('#occupationFilter2').is(":checked") || $('#occupationFilter3').is(":checked") || $('#occupationFilter4').is(":checked") || $('#occupationFilter5').is(":checked") || $('#occupationFilter6').is(":checked") || $('#occupationFilter7').is(":checked") || $('#occupationFilter8').is(":checked") || $('#occupationFilter9').is(":checked") || $('#occupationFilter10').is(":checked") || $('#occupationFilter11').is(":checked") || $('#occupationFilter12').is(":checked")) {
-			if ($('#occupationFilter0').is(":checked")) 
-				//console.log('your message');
-			else
+			if ($('#occupationFilter0').is(":checked")) {
+				console.log('your message');
+			} else {
 				$(".occupationFilter0").hide();
-								
-			if ($('#occupationFilter1').is(":checked")) 
-				//console.log('your message');
-			else
-				$(".occupationFilter1").hide();
+			}
 			
-			if ($('#occupationFilter2').is(":checked")) 
-				//console.log('your message');	
-			else
+			if ($('#occupationFilter1').is(":checked")) {
+				console.log('your message');
+			} else {
+				$(".occupationFilter1").hide();
+			}
+			
+			if ($('#occupationFilter2').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter2").hide();
-				
-			if ($('#occupationFilter3').is(":checked")) 
-				//console.log('your message');	
-			else
+			}
+			
+			if ($('#occupationFilter3').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter3").hide();
-
-			if ($('#occupationFilter4').is(":checked")) 
-				//console.log('your message');	
-			else
+			}
+			
+			if ($('#occupationFilter4').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter4").hide();
-				
-			if ($('#occupationFilter5').is(":checked")) 
-				//console.log('your message');	
-			else
+			}
+			
+			if ($('#occupationFilter5').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter5").hide();
-
-			if ($('#occupationFilter6').is(":checked")) 
-				//console.log('your message');	
-			else
+			}
+			
+			if ($('#occupationFilter6').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter6").hide();
-				
-			if ($('#occupationFilter7').is(":checked")) 
-				//console.log('your message');	
-			else
+			}
+			
+			if ($('#occupationFilter7').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter7").hide();
-
-			if ($('#occupationFilter8').is(":checked")) 
-				//console.log('your message');	
-			else
+			}
+			
+			if ($('#occupationFilter8').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter8").hide();
-
-			if ($('#occupationFilter9').is(":checked")) 
-				//console.log('your message');	
-			else
+			}
+			
+			if ($('#occupationFilter9').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter9").hide();
-
-			if ($('#occupationFilter10').is(":checked")) 
-				//console.log('your message');	
-			else
+			}
+			
+			if ($('#occupationFilter10').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter10").hide();
-
-			if ($('#occupationFilter11').is(":checked")) 
-				//console.log('your message');	
-			else
+			}
+			
+			if ($('#occupationFilter11').is(":checked")) {
+				console.log('your message');	
+			} else {
 				$(".occupationFilter11").hide();	
-				
-			if ($('#occupationFilter12').is(":checked")) 
-				//console.log('your message');	
-			else
-				$(".occupationFilter12").hide();	
+			}
+			
+			if ($('#occupationFilter12').is(":checked")) {
+				console.log('your message');	
+			} else {
+				$(".occupationFilter12").hide();
+			}
+			
 		} else {
 			console.log('all occupation false');
 		}
