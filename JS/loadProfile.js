d35bceb07c0b485355d4d9a3f4d69193a032f4ce
Harish Kotkar <35762074+kotkar321@users.noreply.github.com>
2020-05-25 01:12:42 +0530

    Update loadProfile.js

--- JS/loadProfile.js
+++ JS/loadProfile.js
@@ -60,7 +60,7 @@ function fetchProfiles(profileId, total, pageName, language) {
         $.ajax({
             url: "profiles/groom/" + profileId + ".json", 
             success: function(result){
-                var profileData = this.result;
+                var profileData = result;
                 console.log("Profile :" + profileData);
                 var profile = JSON.parse(profileData);
                 var myStr = "<div class='col-md-4 GOTRA OCCUPATION CITY all'><div class='row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative'><div class='col p-4 d-flex flex-column position-static'><strong class='d-inline-block text-success'>TYPE</strong><h3 class='mb-0'>NAME</h3><div class='mb-1 text-muted'>BIRTHDATE</div><div class=''>Height : <b><i>HEIGHT</i></b> </div><div class=''>Gotra : <b><i>GOTRA</i></b> </div><div class=''>Occupation : <b><i>OCCUPATION</i></b></div><div class=''>Work City : <b><i>CITY </i></b></div><a href='URL' class='' id='linkInfo'> More Info... </a></div></div></div>";
